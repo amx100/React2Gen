8765e6058800f931ab37353337de7eb1496f2224
Ahned <amx281@gmail.com>
2022-07-08 18:02:48 +0200

    Added new lines of code

---
+++ src/components/class/Countdown.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+class Countdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  componentDidMount() {}
+
+  componentWillUnmount() {}
+
+  render() {
+    return <p>Text</p>;
+  }
+}
+
+export default Countdown;
---
+++ src/components/class/Ping.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+class Ping extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+  componentDidMount() {
+    console.log("first");
+  }
+  componentWillUnmount() {}
+  render() {
+    return (
+      <div>
+        <p>{this.state.date.toTimeString()}</p>
+      </div>
+    );
+  }
+}
+export default Ping;
