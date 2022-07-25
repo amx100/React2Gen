0c9a9bf31c71d4307f8c357bdcb6ed696fd7cb21
Ahmed <amx281@gmail.com>
2022-07-25 22:40:30 +0100

    main button

--- src/components/functional/MainButton/index.jsx
+++ src/components/functional/MainButton/index.jsx
@@ -1,64 +1,65 @@
-// import style from "./mainButton.module.css";
+import style from "./mainButton.module.css";
 
-// const MainButton = (props) => {
-//   const { text = "Click me", color = "dark", radius, size } = props;
+const MainButton = (props) => {
+  const { text = "Click me", color = "dark", radius, size } = props;
 
-//   const getColor = () => {
-//     switch (color) {
-//       case "dark":
-//         return style.main;
-//       case "red":
-//         return style.red;
-//       case "yellow":
-//         return style.yellow;
-//       case "white":
-//         return style.white;
-//       default:
-//         return style.main;
-//     }
-//   };
+  const getColor = () => {
+    switch (color) {
+      case "dark":
+        return style.main;
+      case "red":
+        return style.red;
+      case "yellow":
+        return style.yellow;
+      case "white":
+        return style.white;
+      default:
+        return style.main;
+    }
+  };
 
-//   const getRadius = () => {
-//     switch (radius) {
-//       case "xs":
-//         return style.radiusXS;
-//       case "sm":
-//         return style.radiusSM;
-//       case "md":
-//         return style.radiusMD;
-//       case "lg":
-//         return style.radiusLG;
-//       case "xl":
-//         return style.radiusXL;
-//       default:
-//         return style.radiusNull;
-//     }
-//   };
+  const getRadius = () => {
+    switch (radius) {
+      case "xs":
+        return style.radiusXS;
+      case "sm":
+        return style.radiusSM;
+      case "md":
+        return style.radiusMD;
+      case "lg":
+        return style.radiusLG;
+      case "xl":
+        return style.radiusXL;
+      default:
+        return style.radiusNull;
+    }
+  };
 
-//   const getSize = () => {
-//     switch (size) {
-//       case "xs":
-//         return style.sizeXS;
-//       case "sm":
-//         return style.sizeSM;
-//       case "md":
-//         return style.sizeMD;
-//       case "lg":
-//         return style.sizeLG;
-//       case "xl":
-//         return style.sizeXL;
-//       default:
-//         return style.normal;
-//     }
-//   };
+  const getSize = () => {
+    switch (size) {
+      case "xs":
+        return style.sizeXS;
+      case "sm":
+        return style.sizeSM;
+      case "md":
+        return style.sizeMD;
+      case "lg":
+        return style.sizeLG;
+      case "xl":
+        return style.sizeXL;
+      default:
+        w;
+        return style.normal;
+    }
+  };
 
-//   <input type="checkbox" />;
+  <input type="checkbox" />;
 
-//   return (
-//     <button className={`${getColor()} ${getSize()} ${getRadius()}`}>
-//       {text}
-//     </button>
-//   );
-// };
+  return (
+    <button className={`${getColor()} ${getSize()} ${getRadius()}`}>
+      {text}
+    </button>
+  );
+};
 
-// export default MainButton;
+export default MainButton;
