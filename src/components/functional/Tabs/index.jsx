1041cccc38cb12a3c9b1b361ca927c5c263a88c4
Ahned <amx281@gmail.com>
2022-07-26 23:45:52 +0200

    Tabs

---
+++ src/components/functional/Tabs/index.jsx
@@ -0,0 +1,18 @@
+import style from "/tabs.module.css";
+const Tabs = (props) => {
+  return (
+    <div className={style.tabsContainer}>
+      <div>
+        <p>Title!</p>
+      </div>
+      <div>
+        <p>Title!</p>
+      </div>
+      <div>
+        <p>Title!</p>
+      </div>
+    </div>
+  );
+};
+
+export default Tabs;
